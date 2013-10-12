c87f3aaf0f0c0f57ffc6c6e4a258114b39dd9f59
Simo Rinne <rinne.simo@gmail.com>
2013-10-12 16:06:48 +0300

    yksikon valinnan piirto tehty

--- Game/Client.hs
+++ Game/Client.hs
@@ -4,12 +4,14 @@ import Control.Applicative
 import Game.Position
 import qualified Game.GameWorld as G
 import qualified Game.Resources as R
+import qualified Game.Unit as U
 
 -- | Client kuvaa koko asiakasohjelman tilaa
 data Client = Client {
     resources :: R.Resources,
     gameworld :: G.GameWorld,
-    mousePos  :: Position
+    mousePos  :: Position,
+    selectedUnit :: Maybe U.Unit
 }
 
 -- | Luo uuden clientin ja lataa sille resurssit
@@ -18,3 +20,4 @@ newClient = Client
          <$> R.loadResources
          <*> return G.initialGameWorld
          <*> return (0, 0)
+         <*> return Nothing
