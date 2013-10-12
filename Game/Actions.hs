e625f8427a7ec7b8b98a67cd6b953bbb8c82fd6e
Aleksi Pekkala <aleksipekkala@hotmail.com>
2013-10-12 14:51:11 +0300

    Actionit jatkuu

--- Game/Actions.hs
+++ Game/Actions.hs
@@ -4,15 +4,17 @@ import GameWorld (GameWorld, updateUnit)
 import Unit (Unit, Position, moveUnit)
 import Tile (Tile)
 
+
 -- | Siirtää yksikön annettuun paikkaan.
 -- Huom! Oikeellisuustarkistukset tehdään aiemmin
 -- (ei pitäis olla päällekkäisyyksiä)
 move :: Unit -> Position -> GameWorld -> GameWorld
 move u pos gw = updateUnit gw (moveUnit u pos)
 
--- | Huitasee ukkelia
+-- | Huitasee ukkelia.
 smack :: GameWorld -> Unit -> Unit -> GameWorld
 smack gw u1 u2 = undefined
+  where (perc1, perc2) = getPercentages u1 u2 gw
 
-getPercentage :: Unit -> Unit -> Tile -> Float
-getPercentage att def t = undefined
+getPercentages :: Unit -> Unit -> GameWorld -> (Float, Float)
+getPercentages att def gw = undefined
